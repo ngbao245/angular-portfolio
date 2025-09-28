d9799535eaec7412e42e2839af0b3209155b344d
BaoBiBo <ng.hoangbao03@gmail.com>
2025-09-28 19:30:57 +0700

    Bypass SSR routes under admin

--- src/server.ts
+++ src/server.ts
@@ -35,34 +35,40 @@ app.use(
   }),
 );
 
-/**
- * Handle all other requests by rendering the Angular application.
- */
-app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
-
-/**
- * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-
-    console.log(`Node Express server listening on http://localhost:${port}`);
+// CSR fallback cho /admin/**
+app.get(['/admin*'], (req, res) => {
+  app.use('/admin', (req, res) => {
+    res.sendFile(join(browserDistFolder, 'index.html'));
   });
-}
 
-/**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
- */
-export const reqHandler = createNodeRequestHandler(app);
+  /**
+   * Handle all other requests by rendering the Angular application.
+   */
+  app.use((req, res, next) => {
+    angularApp
+      .handle(req)
+      .then((response) =>
+        response ? writeResponseToNodeResponse(response, res) : next(),
+      )
+      .catch(next);
+  });
+
+  /**
+   * Start the server if this module is the main entry point.
+   * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+   */
+  if (isMainModule(import.meta.url)) {
+    const port = process.env['PORT'] || 4000;
+    app.listen(port, (error) => {
+      if (error) {
+        throw error;
+      }
+
+      console.log(`Node Express server listening on http://localhost:${port}`);
+    });
+  }
+
+  /**
+   * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+   */
+  export const reqHandler = createNodeRequestHandler(app);

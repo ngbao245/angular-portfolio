6a16b7b39c85c1a544b7c40d6f575c8d097f872a
BaoBiBo <ng.hoangbao03@gmail.com>
2025-09-29 22:51:56 +0700

    Set up application layout

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { MainLayoutComponent } from '../../shared/layouts/main-layout/main-layout.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent { }
